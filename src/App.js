6eba97181afc4472350ab1b3cf41ebeb948315c1
Le Mong Thuy <thuyle2000@gmail.com>
2024-01-06 11:46:31 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const noel = window.location.origin + "/images/noel1.jpg";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={noel} className="App-logo"  alt='noel' />
+
         <a
           className="App-link"
           href="https://reactjs.org"
